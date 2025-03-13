82492994912e5b61d508e51c942801215ec69b79
SiyingLiu03 <124643854+SiyingLiu03@users.noreply.github.com>
2025-03-12 21:50:26 -0700

    Update SQL_Script.sql
    just load California dataset to test whether I can upload it or not

--- SQL_Script.sql
+++ SQL_Script.sql
@@ -1,39 +1,30 @@
--- Create table for counties data
-CREATE TABLE counties_data (
-    Year INT,
-    County VARCHAR(255),
-    PQI INT,
-    PQIDescription TEXT,
-    Count_ICD10 INT,
-    Population_ICD10 INT,
-    ObsRate_ICD10 FLOAT
+drop table california;
+
+
+CREATE TABLE california (
+    Year int,
+    State varchar(2),
+    Rate float,
+    Deaths int
 );
 
--- Load data into counties_data
-LOAD DATA INFILE 'C/Users/11028/Desktop/counties.csv'
-INTO TABLE counties_data
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(Year, County, PQI, PQIDescription, 
-Count_ICD10, @Population_ICD10, ObsRate_ICD10)
-SET Population_ICD10 = REPLACE(@Population_ICD10, ',', '');
+insert into california
+values('2022','CA','25','11618')
 
--- Create table for California state data
-CREATE TABLE california_data (
-    Year INT,
-    State VARCHAR(2),
-    Rate FLOAT,
-    Deaths INT
-);
+insert into california
+values('2021','CA','25.5','11440')
 
--- Load data into california_data
-LOAD DATA INFILE 'C/Users/11028/Desktop/california.csv'
-INTO TABLE california_data
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(Year, State, Rate, @Deaths)
-SET Deaths = REPLACE(@Deaths, ',', '');
+insert into california
+values('2020','CA','25.4','11642')
+
+insert into california
+values('2019','CA','21.8','9854')
+
+insert into california
+values('2018','CA','21.4','9506')
+
+insert into california
+values('2017','CA','22.1','9595')
+
+insert into california
+values('2016','CA','21.4','9124')
